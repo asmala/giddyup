8ce20eee235a45c40015908c2eca1f74f05b52b4
Janne Asmala <janne@asmala.name>
2014-04-08 08:51:49 +0800

    Clarified dependencies

--- project.clj
+++ project.clj
@@ -4,10 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[hiccup "1.0.5"]]
-  :profiles {:1.3  {:dependencies [[org.clojure/clojure "1.3.0"]]}
+  :profiles {:1.2  {:dependencies [[org.clojure/clojure "1.2.1"]]}
+             :1.3  {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :dev  {:plugins [[codox "0.6.7"]]}}
-  :aliases {"test-all" ["with-profile" "test:test,1.3:test,1.4:test,1.5:test,1.6" "test"]}
+             :doc  {:dependencies [[org.clojure/clojure "1.6.0"]]
+                    :plugins [[codox "0.6.7"]]}}
+  :aliases {"test-all" ["with-profile" "1.2:1.3:1.4:1.5:1.6" "test"]}
   ::min-lein-version "2.0.0")
